a84889d79ddb151e34d51108c60b4a417520390a
appleseedexm <appleseedexm@gmail.com>
2024-05-13 22:29:10 +0200

    adding manual_dirs.as_strings

--- src/dirs.rs
+++ src/dirs.rs
@@ -5,12 +5,19 @@ use error_stack::{Result, ResultExt};
 
 pub trait DirContainer {
     fn find_dir(&self, name: &str) -> Option<&PathBuf>;
+    fn as_strings(&self) -> Vec<String>;
 }
 
 impl DirContainer for Vec<PathBuf> {
     fn find_dir(&self, name: &str) -> Option<&PathBuf> {
         self.iter()
-            .find(|&&path| path.as_os_str().to_str().unwrap() == name)
+            .find(|&path| path.as_os_str().to_str().unwrap() == name)
+    }
+
+    fn as_strings(&self) -> Vec<String> {
+        self.iter()
+            .map(|path| path.as_os_str().to_str().unwrap().to_string())
+            .collect()
     }
 }
 
@@ -18,6 +25,7 @@ pub fn manual_dirs(manual_dirs: Option<Vec<String>>) -> Result<Option<Vec<PathBu
     match manual_dirs {
         Some(x) => Ok(Some(
             x.iter()
+            // todo: check error type
                 .map(|path| canonicalize(path).change_context(TmsError::IoError))
                 .collect::<Result<Vec<PathBuf>, TmsError>>()?,
         )),
