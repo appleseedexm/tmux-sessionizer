f3996c4165df12c194942794cc853f21e1fbb251
appleseedexm <appleseedexm@gmail.com>
2024-05-13 22:59:05 +0200

    started extracting main repo code

--- src/main.rs
+++ src/main.rs
@@ -57,54 +57,54 @@ fn main() -> Result<(), TmsError> {
         return Ok(());
     };
 
-    if let Some(dirs_name) = manual_dirs.find_dir(str){
-        // todo : manual dirs
-    }
+    //if let Some(dirs_name) = manual_dirs.find_dir(str){
+        //// todo : manual dirs
+    //}
 
-    // todo: the found repo is still relevant down the line... 
-    let (found_repo, path) = fun_name(repos, &repo_name)?;
-    let repo_short_name = repo_short_name(config, repo_name)?;
+    //// todo: the found repo is still relevant down the line... 
+    //let (found_repo, path) = fun_name(repos, &repo_name)?;
+    //let repo_short_name = repo_short_name(config, repo_name)?;
 
-    switch(repo_short_name, tmux, path, found_repo)?;
+    //switch(repo_short_name, tmux, path, found_repo)?;
 
     Ok(())
 }
 
-fn fun_name(repos: impl RepoContainer, repo_name: &String) -> Result<(&git2::Repository, String), Report<TmsError>> {
-    let found_repo = repos
-        .find_repo(repo_name)
-        .expect("The internal representation of the selected repository should be present");
-    let path = if found_repo.is_bare() {
-        found_repo.path().to_string()?
-    } else {
-        found_repo
-            .workdir()
-            .expect("bare repositories should all have parent directories")
-            .canonicalize()
-            .change_context(TmsError::IoError)?
-            .to_string()?
-    };
-    Ok((found_repo, path))
-}
+//fn fun_name(repos: impl RepoContainer, repo_name: &String) -> Result<(&git2::Repository, String), Report<TmsError>> {
+    //let found_repo = repos
+        //.find_repo(repo_name)
+        //.expect("The internal representation of the selected repository should be present");
+    //let path = if found_repo.is_bare() {
+        //found_repo.path().to_string()?
+    //} else {
+        //found_repo
+            //.workdir()
+            //.expect("bare repositories should all have parent directories")
+            //.canonicalize()
+            //.change_context(TmsError::IoError)?
+            //.to_string()?
+    //};
+    //Ok((found_repo, path))
+//}
 
-fn switch(repo_short_name: String, tmux: Tmux, path: String, found_repo: &git2::Repository) -> Result<(), Report<TmsError>> {
-    if !session_exists(&repo_short_name, &tmux) {
-        tmux.new_session(Some(&repo_short_name), Some(&path));
-        set_up_tmux_env(found_repo, &repo_short_name, &tmux)?;
-    }
-    switch_to_session(&repo_short_name, &tmux);
-    Ok(())
-}
+//fn switch(repo_short_name: String, tmux: Tmux, path: String, found_repo: &git2::Repository) -> Result<(), Report<TmsError>> {
+    //if !session_exists(&repo_short_name, &tmux) {
+        //tmux.new_session(Some(&repo_short_name), Some(&path));
+        //set_up_tmux_env(found_repo, &repo_short_name, &tmux)?;
+    //}
+    //switch_to_session(&repo_short_name, &tmux);
+    //Ok(())
+//}
 
-fn repo_short_name(config: Box<tms::configs::Config>, repo_name: String) -> Result<String, Report<TmsError>> {
-    let repo_short_name = (if config.display_full_path == Some(true) {
-        std::path::PathBuf::from(&repo_name)
-            .file_name()
-            .expect("None of the paths here should terminate in `..`")
-            .to_string()?
-    } else {
-        repo_name
-    })
-    .replace('.', "_");
-    Ok(repo_short_name)
-}
+//fn repo_short_name(config: Box<tms::configs::Config>, repo_name: String) -> Result<String, Report<TmsError>> {
+    //let repo_short_name = (if config.display_full_path == Some(true) {
+        //std::path::PathBuf::from(&repo_name)
+            //.file_name()
+            //.expect("None of the paths here should terminate in `..`")
+            //.to_string()?
+    //} else {
+        //repo_name
+    //})
+    //.replace('.', "_");
+    //Ok(repo_short_name)
+//}
